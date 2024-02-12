b737ae2e841415b743bad0cc0defae2cb3f50dec
unknown <pratikjojode2004@gmail.com>
2024-02-12 12:56:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function OpenNav(){
+    const nav=document.querySelector("#nav2");
+    nav.style.display="flex";
+}
+function CloseNav(){
+    const nav=document.querySelector("#nav2");
+    nav.style.display="none";
+}
